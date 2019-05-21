41aba43efdf7e2e348fafa43ba60acde5ca355a6
Suhyeon Kim <wisemuji@gmail.com>
2019-05-21 12:08:52 +0900

    #2 리스트박스에 새로운 값 추가

--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -12,9 +12,55 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        private string OrgStr = ""; // 결과 : 저장
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lbResult.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            processInput();
+        }
+
+        private void LbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lbResult.Text = this.OrgStr + this.lbView.Text;
+        }
+
+        private bool processInput()
+        {
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("추가할 항목을 입력해주세요", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtList.Focus();
+                return false;
+            }
+            else
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.txtList.Focus();
+                return true;
+            }
+        }
+
+        private void TxtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (processInput())
+                {
+                    e.Handled = true;
+                }
+            }
         }
     }
 }
